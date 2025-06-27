92db2ceccc4ae4552209475bafb1966b6d0eaf67
RecheEduardo <Eduardorechemartins2006@gmail.com>
2025-06-27 18:41:19 -0300

    style: defininido paleta de cores que serao utilizadas nos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,17 @@ function App() {
   return (
     <>
       <h1 className='text-3xl font-bold underline'>Tudo certo, funcionando!!</h1>
+
+      {/* teste temporario da paleta definida no root do tailwind */}
+      <div className="bg-neutral-background p-8">
+        <h1 className="text-neutral-text text-2xl">Produtos Relacionados</h1>
+          <button className="bg-primary-yellow text-primary-black font-bold py-2 px-4 rounded">
+            Ver Promoção
+          </button>
+          <button className="bg-primary-blue text-primary-white font-bold py-2 px-4 rounded ml-4">
+            Comprar
+          </button>
+      </div>
     </>
   )
 }
