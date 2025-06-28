17c9cfabc19eb38ccdc64e15958a32c8d13b01f5
RecheEduardo <Eduardorechemartins2006@gmail.com>
2025-06-28 12:51:54 -0300

    feature: navbar de categorias com mapeamento dinâmico de tópicos

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { FaReact } from "react-icons/fa"
 import { TopBar } from "./components/layout/TopBar"
 import {MainHeader} from "./components/layout/MainHeader"
+import { NavBar } from "./components/layout/NavBar"
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <TopBar />
       <MainHeader />
+      <NavBar/>
       <h1 className='text-3xl font-bold underline'><FaReact size={90}/>Tudo certo, funcionando!!</h1>
 
       {/* teste temporario da paleta definida no root do tailwind */}
