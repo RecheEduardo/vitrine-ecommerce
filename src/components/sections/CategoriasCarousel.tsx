74dab8b6640849d30b9f10ec983e50865d900b1c
RecheEduardo <Eduardorechemartins2006@gmail.com>
2025-06-28 20:23:01 -0300

    feat: usando os ícones de react-icons e o estilo condicional de item ativo

--- src/components/sections/CategoriasCarousel.tsx
+++ src/components/sections/CategoriasCarousel.tsx
@@ -1,14 +1,21 @@
+import { LuMonitorSmartphone } from "react-icons/lu";
+import { IoStorefrontOutline } from "react-icons/io5";
+import { GiWineBottle, GiClothes } from "react-icons/gi";
+import { FaTools } from "react-icons/fa";
+import { PiHandHeart } from "react-icons/pi";
+import { IoFitness } from "react-icons/io5";
+
 // importando para poder utilizar um componente react como type
 import type { ReactNode } from "react";
 
-const categorias = [
-  { nome: 'Tecnologia', icone: null, ativo: true },
-  { nome: 'Supermercado', icone: null, ativo: false },
-  { nome: 'Bebidas', icone: null, ativo: false },
-  { nome: 'Ferramentas', icone: null, ativo: false },
-  { nome: 'Saúde', icone: null, ativo: false },
-  { nome: 'Esportes e Fitness', icone: null, ativo: false },
-  { nome: 'Moda', icone: null, ativo: false },
+const categorias = [ // array de categorias para renderização dinamica
+  { nome: 'Tecnologia', icone: <LuMonitorSmartphone size={65} />, ativo: true },
+  { nome: 'Supermercado', icone: <IoStorefrontOutline size={65} />, ativo: false },
+  { nome: 'Bebidas', icone: <GiWineBottle size={65} />, ativo: false },
+  { nome: 'Ferramentas', icone: <FaTools size={65} />, ativo: false },
+  { nome: 'Saúde', icone: <PiHandHeart size={65} />, ativo: false },
+  { nome: 'Esportes e Fitness', icone: <IoFitness size={65} />, ativo: false },
+  { nome: 'Moda', icone: <GiClothes size={65} />, ativo: false },
 ];
 
 // tipagem dos objetos
@@ -18,18 +25,21 @@ type CategoryItemProps = {
   ativo: boolean;
 };
 
-const CategoryItem: React.FC<CategoryItemProps> = ({ nome, icone }) => (
+const CategoryItem: React.FC<CategoryItemProps> = ({ nome, icone, ativo }) => (
   <div className="flex flex-col items-center gap-4 flex-shrink-0">
-    <div className="w-36 h-36 flex items-center justify-center rounded-lg shadow-lg">
+    <div
+      className={`w-36 h-36 flex items-center justify-center rounded-lg 
+      shadow-lg ${ativo ? 'text-dark-blue' : 'text-gray-400'}`}
+    >
       {icone}
     </div>
-    <span className="text-sm font-medium text-gray-400">
+    <span className={`text-sm ${ativo ? 'text-dark-blue font-extrabold' : 'text-gray-400 font-medium'}`}>
       {nome}
     </span>
   </div>
 );
 
-export const CategoriasCarousel: React.FC = () => {
+export const CategoriasCarousel = () => {
   return (
     <section className="py-12 bg-white">
       <div className="container mx-auto px-4">
