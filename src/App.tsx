edd127e482ed6706c781ed89ab3b1f48db742978
RecheEduardo <Eduardorechemartins2006@gmail.com>
2025-06-28 11:34:05 -0300

    style: refatoração das paletas no app & icone do site adicionado

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,12 @@ function App() {
       <h1 className='text-3xl font-bold underline'><FaReact size={90}/>Tudo certo, funcionando!!</h1>
 
       {/* teste temporario da paleta definida no root do tailwind */}
-      <div className="bg-neutral-background p-8">
-        <h1 className="text-neutral-text text-2xl">Produtos Relacionados</h1>
-          <button className="bg-primary-yellow text-primary-black font-bold py-2 px-4 rounded">
+      <div className="bg-gray-100 p-8">
+        <h1 className="text-2xl">Produtos Relacionados</h1>
+          <button className="bg-secondary text-white font-bold py-2 px-4 rounded">
             Ver Promoção
           </button>
-          <button className="bg-primary-blue text-primary-white font-bold py-2 px-4 rounded ml-4">
+          <button className="bg-dark-blue text-white font-bold py-2 px-4 rounded ml-4">
             Comprar
           </button>
       </div>
