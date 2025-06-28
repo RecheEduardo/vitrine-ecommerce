4e01e37efda9e4061cfd2051e6326c499f43788d
RecheEduardo <Eduardorechemartins2006@gmail.com>
2025-06-28 19:57:20 -0300

    style: asset da imagem do background integrado na hero

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -1,20 +1,20 @@
 export const HeroSection = () => {
-    return (
-      <section className="relative w-full py-30 bg-primary">
+  return (
+    <section className="relative w-full py-20 bg-cover bg-center bg-no-repeat" style={{ backgroundImage: "url('/heroSectionBackground.jpg')" }}>
+           
+      <div className="relative container mx-auto px-4 flex flex-col justify-center gap-4 items-start text-white">
+        <h1 className="text-4xl md:text-5xl font-bold max-w-3xl">
+          Venha conhecer nossas promoções
+        </h1>
         
-        <div className="relative container mx-auto px-4 flex flex-col justify-center gap-4 items-start text-white">
-          <h1 className="text-4xl md:text-5xl font-bold max-w-3xl">
-            Venha conhecer nossas promoções
-          </h1>
-          
-          <p className="text-4xl mt-4">
-            <span className="text-secondary font-semibold">50% Off</span> nos produtos
-          </p>
-          
-          <a href="#" className="mt-10 bg-secondary text-black font-bold py-4 px-16 rounded-md hover:bg-yellow-300 transition-colors">
-            Ver produto
-          </a>
-        </div>
-      </section>
-    );
+        <p className="text-4xl mt-4">
+          <span className="text-secondary font-semibold">50% Off</span> nos produtos
+        </p>
+        
+        <a href="#" className="mt-10 bg-secondary text-black font-bold py-4 px-16 rounded-md hover:bg-yellow-300 transition-colors">
+          Ver produto
+        </a>
+      </div>
+    </section>
+  );
 };
