878418c02bd8109a61fd6208aca299bd20f919cd
RecheEduardo <Eduardorechemartins2006@gmail.com>
2025-06-28 20:24:20 -0300

    style: efeitos de hover e transição no CategoryItem

--- src/components/sections/CategoriasCarousel.tsx
+++ src/components/sections/CategoriasCarousel.tsx
@@ -6,33 +6,37 @@ import { PiHandHeart } from "react-icons/pi";
 import { IoFitness } from "react-icons/io5";
 
 // importando para poder utilizar um componente react como type
-import type { ReactNode } from "react";
+import type { ReactNode } from "react"
 
 const categorias = [ // array de categorias para renderização dinamica
-  { nome: 'Tecnologia', icone: <LuMonitorSmartphone size={65} />, ativo: true },
-  { nome: 'Supermercado', icone: <IoStorefrontOutline size={65} />, ativo: false },
-  { nome: 'Bebidas', icone: <GiWineBottle size={65} />, ativo: false },
-  { nome: 'Ferramentas', icone: <FaTools size={65} />, ativo: false },
-  { nome: 'Saúde', icone: <PiHandHeart size={65} />, ativo: false },
-  { nome: 'Esportes e Fitness', icone: <IoFitness size={65} />, ativo: false },
-  { nome: 'Moda', icone: <GiClothes size={65} />, ativo: false },
-];
+  {nome: 'Tecnologia',icone: <LuMonitorSmartphone size={65} />,ativo: true},
+  {nome: 'Supermercado',icone: <IoStorefrontOutline size={65}/>,ativo: false},
+  {nome: 'Bebidas', icone: <GiWineBottle size={65} />,ativo: false},
+  {nome: 'Ferramentas',icone: <FaTools size={65}/>, ativo: false},
+  {nome: 'Saúde', icone: <PiHandHeart size={65} />,ativo: false},
+  {nome: 'Esportes e Fitness', icone: <IoFitness size={65}/>,ativo: false},
+  {nome: 'Moda',icone: <GiClothes size={65}/>, ativo: false},
+]
 
 // tipagem dos objetos
 type CategoryItemProps = {
   nome: string;
   icone: ReactNode;
   ativo: boolean;
-};
+}
 
 const CategoryItem: React.FC<CategoryItemProps> = ({ nome, icone, ativo }) => (
-  <div className="flex flex-col items-center gap-4 flex-shrink-0">
+  <div className="flex flex-col items-center gap-4 flex-shrink-0 cursor-pointer
+   hover:scale-90 transition-all duration-200"
+  >
+    {/* inserção de classes condicional a partir do boolean de ativo no tipo criado */}
     <div
       className={`w-36 h-36 flex items-center justify-center rounded-lg 
       shadow-lg ${ativo ? 'text-dark-blue' : 'text-gray-400'}`}
     >
       {icone}
     </div>
+
     <span className={`text-sm ${ativo ? 'text-dark-blue font-extrabold' : 'text-gray-400 font-medium'}`}>
       {nome}
     </span>
@@ -41,19 +45,19 @@ const CategoryItem: React.FC<CategoryItemProps> = ({ nome, icone, ativo }) => (
 
 export const CategoriasCarousel = () => {
   return (
-    <section className="py-12 bg-white">
-      <div className="container mx-auto px-4">
-        <div className="flex gap-12 justify-center overflow-x-auto pb-4 -mb-4">
-          {categorias.map((categoria) => (
-            <CategoryItem
-              key={categoria.nome}
-              nome={categoria.nome}
-              icone={categoria.icone}
-              ativo={categoria.ativo}
-            />
-          ))}
-        </div>
-      </div>
-    </section>
+      <section className="py-12 bg-white">
+          <div className="container mx-auto px-4">
+              <div className="flex gap-12 justify-center overflow-x-auto pb-4 -mb-4">
+                  {categorias.map((categoria) => ( // map renderizando os objetos como props
+                      <CategoryItem
+                          key={categoria.nome}
+                          nome={categoria.nome}
+                          icone={categoria.icone}
+                          ativo={categoria.ativo}
+                      />
+                  ))}
+              </div>
+          </div>
+      </section>
   );
 };
