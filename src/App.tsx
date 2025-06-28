e6bd2d4295a6ca8de255ef933c073d117bb8683c
RecheEduardo <Eduardorechemartins2006@gmail.com>
2025-06-28 12:57:41 -0300

    chore: removendo placeholder inicial de teste de importações

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import { FaReact } from "react-icons/fa"
 import { TopBar } from "./components/layout/TopBar"
 import {MainHeader} from "./components/layout/MainHeader"
 import { NavBar } from "./components/layout/NavBar"
+
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <TopBar />
       <MainHeader />
       <NavBar/>
-      <h1 className='text-3xl font-bold underline'><FaReact size={90}/>Tudo certo, funcionando!!</h1>
-
-      {/* teste temporario da paleta definida no root do tailwind */}
-      <div className="bg-gray-100 p-8">
-        <h1 className="text-2xl">Produtos Relacionados</h1>
-          <button className="bg-secondary text-white font-bold py-2 px-4 rounded">
-            Ver Promoção
-          </button>
-          <button className="bg-dark-blue text-white font-bold py-2 px-4 rounded ml-4">
-            Comprar
-          </button>
-      </div>
+      
     </div>
   )
 }
