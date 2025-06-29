cf815f9e93717a6ed7548f0c747c70a7349a5062
RecheEduardo <Eduardorechemartins2006@gmail.com>
2025-06-29 10:34:02 -0300

    feat: menu de categorias dos produtos

--- src/components/sections/SliderProdutos.tsx
+++ src/components/sections/SliderProdutos.tsx
@@ -5,12 +5,22 @@ export const SliderProdutos: React.FC<{title: string}> = ({ title }) => {
 
         {/* titulo customizavel do slider */}
         <div className="flex items-center gap-12">
-          <div className="flex-1 h-px bg-gray-200"></div>
-          <span className="text-4xl font-bold text-dark-blue text-center">{title}</span>
-          <div className="flex-1 h-px bg-gray-200"></div>
+            <div className="flex-1 h-px bg-gray-200"></div>
+            <span className="text-4xl font-bold text-dark-blue text-center">{title}</span>
+            <div className="flex-1 h-px bg-gray-200"></div>
         </div>
 
-        {/* o resto do componente vira aqui */}
+        {/* menu de categorias de produtos */}
+        <div className="flex justify-between gap-6 my-8 px-12 text-lg text-gray-400">
+          <a href="#" className="text-primary font-bold border-b-2 border-primary pb-1">Celular</a>
+          <a href="#" className="hover:text-primary">Acessórios</a>
+          <a href="#" className="hover:text-primary">Tablets</a>
+          <a href="#" className="hover:text-primary">Notebooks</a>
+          <a href="#" className="hover:text-primary">TVs</a>
+          <a href="#" className="hover:text-primary">Ver todos</a>
+        </div>
+        
+        {/* slider sera implementado aqui */}
       </div>
     </section>
   );
