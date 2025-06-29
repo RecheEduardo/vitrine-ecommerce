b629560a47c5e532db9b6df2a67c89be472b72c8
RecheEduardo <Eduardorechemartins2006@gmail.com>
2025-06-28 23:48:46 -0300

    refact: extraindo a logica para o componente IconeMarca & add imagem da econverse

--- src/components/sections/MarcasCarousel.tsx
+++ src/components/sections/MarcasCarousel.tsx
@@ -1,19 +1,22 @@
-export const MarcasCarousel: React.FC = () => {
+const IconeMarca = () => ( // componente iterado no carrossel 
+  <div className="w-64 h-64 flex-shrink-0 bg-white border-0 rounded-full 
+    flex items-center justify-center drop-shadow-xl">
+    {/* usando asset do layout figma da logo da econverse */}
+    <img src="/siteLogo.png" alt="Brand Logo" className="h-12" />
+  </div>
+);
+
+export const MarcasCarousel = () => {
   return (
     <section className="py-12 bg-white">
       <div className="container mx-auto px-4">
-        <h2 className="text-4xl font-bold text-dark-blue text-center mb-8">
-          Navegue por marcas
-        </h2>
+
+        <h2 className="text-4xl font-bold text-dark-blue text-center mb-8">Navegue por marcas</h2>
         
         <div className="relative">
           <div className="flex justify-center gap-12 pb-8 -mb-4 px-12">
-            {[...Array(5)].map((_, i) => ( // mapeando um array (ingessado) para simular 5 logos de marca
-              <div key={i} className="w-48 h-48 flex-shrink-0 bg-gray-200 border-0 rounded-full 
-                flex items-center justify-center">
-                <span className="text-gray-500">Teste</span>
-              </div>
-            ))}
+            {/* mapeando um array (ingessado) para simular 5 logos de marca */}
+            {[...Array(5)].map((_, i) =>  <IconeMarca key={i} />) /* componentizando icones de marcas */}
           </div>
         </div>
         
