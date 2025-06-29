8479434790156a34ff3196ab058c319acd404226
RecheEduardo <Eduardorechemartins2006@gmail.com>
2025-06-29 09:58:54 -0300

    feat: add tratamento de erros com try catch ao hook useProducts

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -6,21 +6,30 @@ const API_URL = '/api/teste-front-end/junior/tecnologia/lista-produtos/produtos.
 export const useProducts = () => {
   // utiliza como tipo um array de produtos, definido em types/
   const [products, setProducts] = useState<Produto[]>();
-  
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const response = await fetch(API_URL);
-      const data = await response.json();
+      try {
+        const response = await fetch(API_URL);
 
-      setProducts(data.products);
-      setLoading(false);
+        if (!response.ok) {
+          throw new Error('ERRO! Falha ao buscar produtos...');
+        }
+
+        const data = await response.json();
+        setProducts(data.products);
+      } catch (err) {
+        // Tratamento de erro ainda genérico
+        setError('Ocorreu um erro ao buscar os produtos.');
+        console.error(err);
+      }
+      setLoading(false); // Será movido para o finally
     };
 
     fetchProducts();
   }, []); // array vazio garante que o fetch ocorra apenas uma vez
-
+  
   return { products, loading, error };
 };
