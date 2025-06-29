7df3e09700f67250c08c9ce3ef75f6d54b8434ae
RecheEduardo <Eduardorechemartins2006@gmail.com>
2025-06-29 09:57:22 -0300

    feat: criando useEffect para fazer o fetch dos produtos na montagem do componente

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -1,12 +1,26 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import type { Produto } from '../types/produto';
 
+const API_URL = '/api/teste-front-end/junior/tecnologia/lista-produtos/produtos.json';
+
 export const useProducts = () => {
+  // utiliza como tipo um array de produtos, definido em types/
   const [products, setProducts] = useState<Produto[]>();
+  
+  const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
-  // logica de fetching assincriona
-  // TODO: tratar estado de loading futuramente
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch(API_URL);
+      const data = await response.json();
 
-  return { products, error };
+      setProducts(data.products);
+      setLoading(false);
+    };
+
+    fetchProducts();
+  }, []); // array vazio garante que o fetch ocorra apenas uma vez
+
+  return { products, loading, error };
 };
