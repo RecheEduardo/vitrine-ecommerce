0c0b6c513df14209ddb4433956998e9f8f89f397
RecheEduardo <Eduardorechemartins2006@gmail.com>
2025-06-28 21:39:45 -0300

    feat: esqueleto da seção newsletter com textos de chamada

---
+++ src/components/sections/Newsletter.tsx
@@ -0,0 +1,20 @@
+export const Newsletter = () => {
+  return (
+    <section className="bg-primary py-12">
+      <div className="container flex justify-between mx-auto py-2 items-center text-gray-100">
+        
+        <div>
+          <h2 className="text-3xl font-bold">Inscreva-se na nossa newsletter</h2>
+          <p className="mt-2 text-white/80 max-w-2xl">Assine a nossa newsletter e receba as
+           novidades e conteúdos exclusivos da Econverse.
+          </p>
+        </div>
+        
+        {/* Formulário a ser implementado */}
+        <div>
+        </div>
+
+      </div>
+    </section>
+  );
+};
