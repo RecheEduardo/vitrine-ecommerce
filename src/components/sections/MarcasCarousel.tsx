9b8ce762efab2d23f9cfe97fb6681445ec81c39e
RecheEduardo <Eduardorechemartins2006@gmail.com>
2025-06-28 23:40:48 -0300

    feat: base do carrossel de marcas

---
+++ src/components/sections/MarcasCarousel.tsx
@@ -0,0 +1,13 @@
+export const MarcasCarousel: React.FC = () => {
+  return (
+    <section className="py-12 bg-white">
+      <div className="container mx-auto px-4">
+        <h2 className="text-4xl font-bold text-dark-blue text-center mb-8">
+          Navegue por marcas
+        </h2>
+        
+        {/* TODO: carrossel das marcas será feito mapeando um array de objetos */}
+      </div>
+    </section>
+  );
+};
