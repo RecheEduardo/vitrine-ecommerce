48b58f2b819b63bee7e982ce9c9834ce8739d6e8
RecheEduardo <Eduardorechemartins2006@gmail.com>
2025-06-29 00:07:30 -0300

    min-feat: markup inical da seção de parceiros com cards

---
+++ src/components/sections/ParceirosSection.tsx
@@ -0,0 +1,17 @@
+const ParceiroCard = () => {
+  return (
+    <div>
+      <h3>Parceiros</h3>
+      <p>Descrição do parceiro</p>
+      <button>Confira</button>
+    </div>
+  );
+};
+
+export const ParceirosSection = () => {
+  return (
+    <section>
+      <ParceiroCard />
+    </section>
+  );
+};
